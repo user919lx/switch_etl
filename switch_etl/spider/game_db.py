815979c1c5fef84466cc25315c913f55354d2653
xinlan <xinlan@tencent.com>
2022-03-22 11:32:12 +0800

    feat:增加game db获取 --other

--- switch_etl/spider/game_db.py
+++ switch_etl/spider/game_db.py
@@ -75,23 +75,3 @@ class GameDBSpider:
             logging.info(f"finish crawl region = {region}")
         logging.info("finish all")
         self.storage.close()
-
-
-# GET /switchtdb.zip?LANG=JA HTTP/1.1
-# Host: www.gametdb.com
-# Connection: keep-alive
-# sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"
-# sec-ch-ua-mobile: ?0
-# sec-ch-ua-platform: "macOS"
-# Upgrade-Insecure-Requests: 1
-# User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36
-# Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
-# Sec-Fetch-Site: none
-# Sec-Fetch-Mode: navigate
-# Sec-Fetch-User: ?1
-# Sec-Fetch-Dest: document
-# Accept-Encoding: gzip, deflate, br
-# Accept-Language: zh-CN,zh;q=0.9
-# Cookie: _ga=GA1.2.276693465.1647497413; _gid=GA1.2.1307277071.1647497413; imstime=1647498480
-# If-None-Match: "6232ebf6-8c4c2"
-# If-Modified-Since: Thu, 17 Mar 2022 08:06:14 GMT
