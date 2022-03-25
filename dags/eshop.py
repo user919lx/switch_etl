f84bc66c5b0ac9df42fcc7d6e0859e7cb81d33d9
xinlan <xinlan@tencent.com>
2022-03-25 17:11:10 +0800

    feat:添加dags --other

---
+++ dags/eshop.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+
+# Operators; we need this to operate!
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    'eshop',
+    # These args will get passed on to each operator
+    # You can override them on a per-task basis during operator initialization
+    default_args={
+        'depends_on_past': False,
+        'email': ['user919lx@gmail.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2021, 3, 24),
+    catchup=False,
+    tags=['eshop'],
+) as dag:
+
+    eshop = BashOperator(
+        task_id='eshop',
+        bash_command='cd /home/ubuntu/Projects/crawler & pipenv shell & scrapy crawl eshop',
+    )
