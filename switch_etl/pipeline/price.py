a941cf941afcf90bccb30b832066db286e5e0a60
xinlan <xinlan@tencent.com>
2022-03-31 12:19:05 +0800

    feat:修改price计算 --other

--- switch_etl/pipeline/price.py
+++ switch_etl/pipeline/price.py
@@ -7,6 +7,7 @@ class PricePipeline(Pipeline):
         self.db.open()
         iter = self.db.record_iter("price_raw", batch_size=10000)
         item_list = []
+        i = 0
         for rows in iter:
             for row in rows:
                 raw_data = json.loads(row[3])
@@ -19,5 +20,7 @@ class PricePipeline(Pipeline):
                     "created_at": row[4],
                 }
                 item_list.append(data)
-        self.db.save_many("price_raw_new", item_list)
+            i += 1
+            print(f"iter {i} times")
+            self.db.save_many("price_raw_new", item_list)
         self.db.close()
